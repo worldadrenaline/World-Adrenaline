cab780b1dae4cd4e8135b8e1b7f75227b2732d70
Ryan Off <ryan.off@kumutu.com>
2010-12-09 19:11:23 +0000

    added core and database files

---
+++ app/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'Ku7r1beDB',
+		'database' => 'worldadrenaline',
+		'encoding' => 'utf8'  
+	);
+}
+?>
