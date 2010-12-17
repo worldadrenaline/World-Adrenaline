5226653a1b50e6322b2ab829ac286dd0b4457d0d
Ryan Off <ryan.off@kumutu.com>
2010-12-17 14:30:05 +0000

    ready for github

---
+++ app/config/templates/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * This file is loaded automatically by the app/webroot/index.php file after the core bootstrap.php is loaded
+ * This is an application wide file to load any function that is not used within a class define.
+ * You can also use this to include or require any files in your application.
+ *
+ */
+ 
+/**
+ * WA Global Constants
+ */ 
+Configure::write('Map.key', '');
+
+/*
+ * Set email delivery address
+ */
+Configure::write('Email.support', 'Support <support@domain.com>');
+
+/*
+ * Set smtp user details
+ */
+Configure::write('Smtp.host', 'mail.domain.com');
+Configure::write('Smtp.username', 'username');
+Configure::write('Smtp.password', 'password');
+
+/*
+ * Affiliate API keys
+ */
+
+
+/**
+ * Affiliate Methods
+ */
+
+  
+?>
